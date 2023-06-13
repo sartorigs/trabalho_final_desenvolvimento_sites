2ad39b234aff436f902f780ae6157908dbc5845a
Gabriel Sartori <86271122+sartorigs@users.noreply.github.com>
2023-06-12 23:17:05 -0300

    criacao git

---
+++ index.js
@@ -0,0 +1,11 @@
+$('#card-calculadora').on('click',function(){
+    window.location.href = 'apps/calculadora.html'
+})
+
+$('#card-imc').on('click',function(){
+    window.location.href = 'apps/imc.html'
+})
+
+$('#card-form').on('click',function(){
+    window.location.href = 'apps/form.html'
+})
